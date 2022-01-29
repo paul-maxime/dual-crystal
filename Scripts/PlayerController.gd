9f9b69559c1fe6ece1680334146c5b609b6d29b4
Maxou <github@paulmaxime.fr>
2022-01-29 14:45:29 +0100

    Check if we can switch worlds, split player and main scene

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -2,6 +2,11 @@ extends KinematicBody2D
 
 var SPEED = 100.0
 
+func is_empty_area(tilemap: TileMap):
+	var cell = tilemap.world_to_map(position)
+	var tile = tilemap.get_cellv(cell)
+	return tile == -1
+
 func swap_color():
 	if collision_layer == 1:
 		$AnimatedSprite.modulate = Color.red
@@ -14,7 +19,11 @@ func swap_color():
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
-		swap_color()
+		if not is_empty_area(get_node("../BlueWorld/Background")):
+			if not is_empty_area(get_node("../RedWorld/Background")):
+				swap_color()
+				if test_move(transform, Vector2.ZERO):
+					swap_color()
 
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_down"):
